99c405372b92958c156a2c4d42e92468e19a07c6
Xiaoxie1994 <xiexiao6@jd.com>
2025-01-24 18:29:38 +0800

    feat(*): fix

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -69,6 +69,10 @@ export default defineUserConfig({
         ],
       },
       {
+        text: '博客',
+        link: 'https://shawnxie.top/',
+      },
+      {
         text: '订阅',
         link: 'https://mp.weixin.qq.com/mp/appmsgalbum?__biz=MzkwODY0ODQzOQ==&action=getalbum&album_id=3492416248238096386#wechat_redirect',
       },
